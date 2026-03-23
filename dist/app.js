724046525b6dde18b55e495f1ad88b189f76525e
Azmaeen Mahtab Ezaz <mahtabazmaeen7@gmail.com>
2026-03-23 16:45:14 +0600

    solved route for get users

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,10 @@
 import express from "express";
 import usersRouter from "./routes/users.js";
-
 const app = express();
-
 app.use(express.json());
 app.use("/users", usersRouter);
-
 app.get("/", (req, res) => {
     res.send("API RUNNING WITH TYPESCRIPT");
 });
-
-
-
 export default app;
+//# sourceMappingURL=app.js.map
