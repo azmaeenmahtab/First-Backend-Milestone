609df908ac87b4cc91a953ecb2435dcc171d0101
Azmaeen Mahtab Ezaz <mahtabazmaeen7@gmail.com>
2026-04-08 23:32:17 +0600

    connecting postgres db

---
+++ src/db/connection.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import {Pool} from "pg"
+dotenv.config(); ///load environment variables form .env file to process.env
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT)
+})
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -4,6 +4,17 @@ const router = Router();
 
 router.post("/postcreate", (req, res) => {
 
+    const { title, description} = req.body;
+
+    if(!title || !description){
+
+        return res.status(400).json({
+            message: "Title and description are required"
+        })
+    }
+
+    
+
     res.json({
         message: "post created successfully"
     });
